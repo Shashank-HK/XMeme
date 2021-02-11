6266aa0b42ecfdf4324fbdd5fb10597ecdef39d8
Shashank HK <shashankhk.1999@gmail.com>
2021-02-11 23:47:57 +0530

    Install change

--- install.sh
+++ install.sh
@@ -14,9 +14,10 @@ sudo systemctl start mongod
 sudo systemctl enable mongod
 
 #install node
+sudo apt purge nodejs npm
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-. ~/.nvm/nvm.sh
 nvm install node
-nvm use node
+
+source ~/.bashrc
